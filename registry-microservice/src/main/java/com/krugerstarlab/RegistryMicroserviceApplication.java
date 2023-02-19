791989f911a3f21d73f45e987ebfb3531c26a5e2
KenanAljaber <keno12333@hotmail.com>
2023-02-19 00:57:54 -0300

    first commit

---
+++ registry-microservice/src/main/java/com/krugerstarlab/RegistryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.krugerstarlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistryMicroserviceApplication.class, args);
+	}
+
+}
