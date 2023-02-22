7d12405662421765a71ac7812b811a6c63d45d92
KenanAljaber <keno12333@hotmail.com>
2023-02-21 22:02:35 -0300

    initial gateway config

--- gateway-microservice/src/main/java/com/krugerstarlab/config/CustomFilter.java
+++ gateway-microservice/src/main/java/com/krugerstarlab/config/CustomFilter.java
@@ -69,7 +69,7 @@ public class CustomFilter implements GlobalFilter {
 		  // Call the userAuth-microservice using WebClient
 		String token=exchange.getRequest().getHeaders().get("Authorization").get(0);
 	    return webClient.get()
-	        .uri("http://USERAUTH-MICROSERVICE/api/v1/users/members/blank")
+	        .uri("http://userAuth-microservice/api/v1/users/members/blank")
 	        .header(HttpHeaders.AUTHORIZATION, token)
 	        .exchange()
 	        .flatMap(response -> {
--- gateway-microservice/src/main/java/com/krugerstarlab/config/GatewayConfig.java
+++ gateway-microservice/src/main/java/com/krugerstarlab/config/GatewayConfig.java
@@ -1,30 +1,16 @@
 package com.krugerstarlab.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class GatewayConfig {
-	private static final Logger logger=LoggerFactory.getLogger(GatewayConfig.class);
-   @Value("${spring.application.name}")
-   private String appName;
 
-   @Bean
-    RouteLocator routeLocator(RouteLocatorBuilder builder) {
-      return builder.routes()
-         .route("registry-microservice", r -> r.path("/")
-            .uri("http://localhost:9091"))
-         .route("USERAUTH-MICROSERVICE",r-> r.path("/api/v1/users/**").uri("http://localhost:8081"))
-         .build();
-   }
+
    
    @Bean
     GlobalFilter globalFilter() {
